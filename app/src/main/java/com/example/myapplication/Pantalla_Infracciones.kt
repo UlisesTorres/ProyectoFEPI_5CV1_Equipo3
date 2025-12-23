60eed874fa0096078250b0c3df505239a144002d
Ulises Torres <angelmzmartinez53@outlook.com>
2025-12-23 01:21:17 -0600

    correccion menor

--- app/src/main/java/com/example/myapplication/Pantalla_Infracciones.kt
+++ app/src/main/java/com/example/myapplication/Pantalla_Infracciones.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
 
 class Pantalla_Infracciones : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
